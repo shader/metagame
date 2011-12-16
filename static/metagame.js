ab9d78c8dddcc47a90224552986fc5b510f14642
Aerosecurity <aerosecurity@gmail.com>
2011-12-16 12:38:17 -0500

    started refactoring main js file to be simpler

--- static/metagame.js
+++ static/metagame.js
@@ -1,65 +1,46 @@
-function metagame() {
+var userInfo = {
+  "username": "",
+  "password": "",
+  "sessionID": ""
+}
 
-	var userInfo = {
-		"username": "",
-		"password": "",
-		"sessionID": ""
-	}
+var isLoggedIn = false
 
-	var isLoggedIn = false
+function alertUserInfo () {
+  alert(userInfo.username + " " + userInfo.password)
+}
 
-	// Init function
-	this.init = function() {
-		if(!isLoggedIn) {
-			this.showLogin()
-		}
-	}
+function showLogin () {
+  $('#login_prompt').slideDown('slow')
+}
 
-	this.setUserInfo = function(name,pass) {
-		userInfo.username = name
-		userInfo.password = pass
-	}
+/* Login function, handles making the call to login
+ */
+this.login = function() {
+  $.ajax({
+    url: "web-login",
+    type: "GET",
+    data: {username: userInfo.username, password: userInfo.password},
+    success: function(data) {
+      console.log("Successful login call")
+    },
+    error: function() {
+      console.log("Error when calling login function")
+    }
+  })
+}
 
-	this.alertUserInfo = function() {
-		alert(userInfo.username + " " + userInfo.password)
-	}
-
-	this.showLogin = function() {
-		$('#login_prompt').slideDown('slow')
-	}
-
-	/* Login function, handles making the call to login
-	 */
-	this.login = function() {
-		$.ajax({
-			url: "web-login",
-			type: "GET",
-			data: {username: userInfo.username, password: userInfo.password},
-			async: false,
-			success: function(data) {
-				console.log("Successful login call")
-			},
-			error: function() {
-				console.log("Error when calling login function")
-			}
-		})
-	}
-
-	this.register = function() {
-		var response = ""
-		$.ajax({
-			url: "web-register",
-			type: "POST",
-			data: {username: userInfo.username, password: userInfo.password},
-			asyn: false,
-			success: function(data) {
-				console.log("Successful register call")
-				response = data
-			},
-			error: function() {
-				console.log("Error when calling register function")
-			}
-		})
-		return response
-	}
+function register () {
+  $.ajax({
+    url: "web-register",
+    type: "POST",
+    data: {username: userInfo.username, password: userInfo.password},
+    success: function(data) {
+      console.log("Successful register call")
+      alert(data)
+    },
+    error: function() {
+      console.log("Error when calling register function")
+    }
+  })
 }
